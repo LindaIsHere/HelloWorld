8ea599a626f71e069458e99e63f0f7ea5af1a0e7
Linda Chuong <Linda Chuong@192.168.2.15>
2021-09-14 22:48:49 -0400

    ICS3U Project of HelloWorld

---
+++ ICS3U-Chuong/src/unit1/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package unit1;
+
+/**
+ * HelloWorld.java
+ * Print Hello World to the screen
+ * Sept 13, 2021
+ * @author Linda Chuong
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hi!!");
+	}
+
+}
