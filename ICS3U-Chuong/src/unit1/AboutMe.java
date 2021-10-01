d50754bdb4c8968a6d46fdec7820bdd0df4784f3
Linda Chuong <Linda Chuong@192.168.2.15>
2021-09-30 23:22:09 -0400

    hi

---
+++ ICS3U-Chuong/src/unit1/AboutMe.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package unit1;
+
+/**
+ * AboutMe.java
+ * @author Linda Chuong
+ * Creat an application about me
+ * Sept 30, 2021
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public AboutMe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Linda C.");
+		System.out.println("Christina K.");
+		System.out.println("Runnymede CI");
+		System.out.println("\"Go Ravens!\"");
+		System.out.println();
+		System.out.println();
+		System.out.printf("%-51s %s\n", "Day 1", "Day 2");
+		System.out.printf("%-50s %s\n", "Time: 9:00AM  Class: Anthropology ", " Time: 9:00AM  Class: Canadian Law");
+		System.out.printf("%-50s %s\n", "Time: 12:15PM  Class: Biology ", " Time: 12:15PM  Class: Tech");
+	}
+
+}
---
+++ ICS3U-Chuong/src/unit1/RecursionExamples.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package unit1;
+
+/**
+ * @author Linda Chuong
+ *
+ */
+public class RecursionExamples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
